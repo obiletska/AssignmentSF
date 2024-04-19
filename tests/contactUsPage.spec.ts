a9d5a6d7f790e188e6802bdd7f4df2709a9f0054
obiletska <olya.biletskaqa@gmail.com>
2024-04-19 13:45:35 -0700

    changes to lifecycle methods

--- tests/contactUsPage.spec.ts
+++ tests/contactUsPage.spec.ts
@@ -1,48 +1,66 @@
-import { test, expect, type Page } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import { ContactUsForm } from './util/contactUsPage';
 import { DataGenerators } from './util/dataGenerators';
 import { faker } from '@faker-js/faker';
 
 
+
 test.beforeEach(async({page}) => {
+
     await page.goto('https://webdriveruniversity.com/Contact-Us/contactus.html')
 })
+
+// generate valid & invalid email before tests 
+let validEmail: string
+let invalidEmail: string
+
+test.beforeEach(async ({ page }) => {
+
+    validEmail = faker.internet.email()
+    console.log(validEmail)
+
+    invalidEmail = validEmail.replace('@', 'at')
+    console.log(invalidEmail)
+});
+
+// take a screenshot after test execution 
+test.afterEach(async({ page }, testInfo) => {
+
+    const testName = page.locator(testInfo.testId)
+    const screenshotPath = `./screenshots/${testName}-${Date.now()}-screenshot.png`
+    await page.screenshot({ path: screenshotPath })
+    
+})
+
 
 // randomNumber generates a random number to use it in randomName to generate random string with lenght = randomNumber
 // randomComment generate string with lenght = 500 symbols
 async function generateValidRandomFormData(dataGenerators: DataGenerators) {
-    const randomNumber = (await dataGenerators.getRandomNumber(2, 26)).valueOf();
-    const randomValidName = (await dataGenerators.generateRandomName(randomNumber)).toString();
-    const randomComment = (await dataGenerators.generateRandomComment(500)).valueOf();
+    const randomNumber = (await dataGenerators.getRandomNumber(2, 26)).valueOf()
+    const randomValidName = (await dataGenerators.generateRandomName(randomNumber)).toString()
+    const randomComment = (await dataGenerators.generateRandomComment(500)).valueOf()
 
-    return { randomValidName, randomComment };
+    return { randomValidName, randomComment }
 }
 
 // generate invalid name (more that 25 symbols)
 async function generateIvnalidRandomFormData(dataGenerators: DataGenerators) {
-    const randomNumber = (await dataGenerators.getRandomNumber(26, 30)).valueOf();
-    const randomInvalidName = (await dataGenerators.generateRandomName(randomNumber)).toString();
+    const randomNumber = (await dataGenerators.getRandomNumber(26, 30)).valueOf()
+    const randomInvalidName = (await dataGenerators.generateRandomName(randomNumber)).toString()
 
-    return { randomInvalidName};
+    return { randomInvalidName}
 }    
-
-// Generate a valid email address
-const validEmail = faker.internet.email();
-console.log(validEmail); 
-
-// Generate an invalid email address
-const invalidEmail = validEmail.replace('@', 'at');
-console.log(invalidEmail); 
 
 test('submit form with valid data', async({page}) => {
 
-    const dataGenerators = new DataGenerators();
-    const { randomValidName, randomComment } = await generateValidRandomFormData(dataGenerators);
+    const dataGenerators = new DataGenerators()
+    const { randomValidName, randomComment } = await generateValidRandomFormData(dataGenerators)
 
     const onContactUsPage = new ContactUsForm(page)
     await onContactUsPage.submitFormWithAllFieldsFilled(randomValidName, randomValidName, validEmail, randomComment)
     const successMessage = page.locator('#contact_reply h1')
     await expect(successMessage).toHaveText("Thank You for your Message!")
+
 })
 
 test('submit form with empty fields', async({page}) => {
@@ -51,12 +69,13 @@ test('submit form with empty fields', async({page}) => {
     await onContactUsPage.submitFormWithAllFieldsFilled('', '', '', '')
     const errorMessage = page.locator('body')
     await expect(errorMessage).toContainText("Error: all fields are required")
+    
 } )
 
 test('submit form with invalid email format', async({page}) => {
 
-    const dataGenerators = new DataGenerators();
-    const { randomValidName, randomComment } = await generateValidRandomFormData(dataGenerators);
+    const dataGenerators = new DataGenerators()
+    const { randomValidName, randomComment } = await generateValidRandomFormData(dataGenerators)
 
     const onContactUsPage = new ContactUsForm(page)
     await onContactUsPage.submitFormWithAllFieldsFilled(randomValidName, randomValidName, invalidEmail, randomComment)
@@ -65,8 +84,8 @@ test('submit form with invalid email format', async({page}) => {
 } )
 
 test('sumbit form with invalid name (long string)', async({page}) => {
-    const dataGenerators = new DataGenerators();
-    const { randomComment } = await generateValidRandomFormData(dataGenerators);
+    const dataGenerators = new DataGenerators()
+    const { randomComment } = await generateValidRandomFormData(dataGenerators)
     const { randomInvalidName } = await generateIvnalidRandomFormData(dataGenerators)
 
     const onContactUsPage = new ContactUsForm(page)
@@ -77,8 +96,8 @@ test('sumbit form with invalid name (long string)', async({page}) => {
 })
 
 test('sumbit form with invalid name (short string)', async({page}) => {
-    const dataGenerators = new DataGenerators();
-    const { randomComment } = await generateValidRandomFormData(dataGenerators);
+    const dataGenerators = new DataGenerators()
+    const { randomComment } = await generateValidRandomFormData(dataGenerators)
 
     const onContactUsPage = new ContactUsForm(page)
     await onContactUsPage.submitFormWithAllFieldsFilled('a', 'a', validEmail, randomComment)
@@ -88,8 +107,8 @@ test('sumbit form with invalid name (short string)', async({page}) => {
 })
 
 test('reset after filling in all information', async({page}) => {
-    const dataGenerators = new DataGenerators();
-    const { randomValidName, randomComment } = await generateValidRandomFormData(dataGenerators);
+    const dataGenerators = new DataGenerators()
+    const { randomValidName, randomComment } = await generateValidRandomFormData(dataGenerators)
 
     const onContactUsPage = new ContactUsForm(page)
     await onContactUsPage.fillFormWithAllFields(randomValidName, randomValidName, validEmail, randomComment)
